fa4d293fc1b6262ac63694c59bd3f6599967f022
adityasrivastava2123-commits <adityasrivastava2123@gmail.com>
2026-05-26 19:50:47 +0530

    Update rateLimit.js

--- server/src/middleware/rateLimit.js
+++ server/src/middleware/rateLimit.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const strictRateLimit = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // limit each IP to 5 requests per windowMs
+  max: 100, // limit each IP to 5 requests per windowMs
   message: "Too many login attempts, please try again later",
   standardHeaders: true,
   legacyHeaders: false,
